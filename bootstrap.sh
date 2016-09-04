55330711c904eccbbf684edcf1eba1fbf00e521e
Amelia Schmidt <milly@Amelias-MacBook-Pro.local>
2016-09-04 16:23:32 +1000

    better safety in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 echo "=> Downloading and bootstrapping..."
-curl -sSL -o /tmp/workstation-setup.zip https://github.com/meelijane/workstation-setup/archive/master.zip
-cd /tmp
-unzip -q workstation-setup.zip
-cd workstation-setup-master
+rm -r /tmp/workstation-setup.zip /tmp/workstation-setup-master;
+curl -sSL -o /tmp/workstation-setup.zip https://github.com/meelijane/workstation-setup/archive/master.zip;
+cd /tmp;
+unzip -q workstation-setup.zip;
+cd workstation-setup-master;
 
 . xcode.sh;
 . ssh-key.sh;
@@ -12,7 +13,7 @@ cd workstation-setup-master
 
 echo "=> Cleaning up temporary files..."
 cd $HOME
-rm -r /tmp/workstation-setup.zip /tmp/workstation-setup-master
+rm -r /tmp/workstation-setup.zip /tmp/workstation-setup-master;
 
 git clone git@github.com:meelijane/workstation-setup.git;
 cd workstation-setup;
