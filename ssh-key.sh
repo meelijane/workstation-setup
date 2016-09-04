601ff017bff4b8d4c7c3b9930ff513a9b1c7d3e3
Amelia Schmidt <milly@Amelias-MacBook-Pro.local>
2016-09-04 15:40:00 +1000

    adding web browsers and making ssh key less verbose

--- ssh-key.sh
+++ ssh-key.sh
@@ -3,9 +3,9 @@
 if [ ! -f ~/.ssh/id_rsa ]; then
   ssh-keygen -t rsa -b 4096;
   ssh-add;
+  echo "=> Your new public ssh key."
+  ssh-add -L;
   echo "=> Log into github and add your SSH key to your account."
   echo "=> Press the any key to continue."
   read;
 fi
-
-ssh-add -L;
---
+++ web-browsers.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+brew cask install google-chrome firefox flash-player;
+
+brew install phantomjs;
