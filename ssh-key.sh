64779f21beb67512ecd893e5a216898eb1d6f903
Amelia Schmidt <milly@Amelias-MacBook-Pro.local>
2016-09-04 16:42:39 +1000

    get ssh-keygen to work better

--- ssh-key.sh
+++ ssh-key.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -f ~/.ssh/id_rsa ]; then
-  ssh-keygen -t rsa -b 4096;
-  ssh-add;
-  ssh-add -L;
+  ssh-keygen -t rsa -b 4096 && ssh-add && ssh-add -L;
   echo "=> Your new public ssh key.";
   echo "=> Log into github and add your SSH key to your account.";
   read -p a
