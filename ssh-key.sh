984c1dfea8948730bbcc313f1b08b37a022f23b5
Amelia Schmidt <milly@Amelias-MacBook-Pro.local>
2016-09-04 16:34:33 +1000

    tweaking read

--- ssh-key.sh
+++ ssh-key.sh
@@ -6,5 +6,5 @@ if [ ! -f ~/.ssh/id_rsa ]; then
   ssh-add -L;
   echo "=> Your new public ssh key.";
   echo "=> Log into github and add your SSH key to your account.";
-  read -p "=> Press the any key to continue." ssh-key-is-in-github;
+  read -p "Press the any key to continue.";
 fi
